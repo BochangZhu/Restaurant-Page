14cddd68860708b3a8886cbf3e2853ee017b011c
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-07-22 17:23:31 -0700

    add homeUtility module skeleton code

--- src/homeUtil.js
+++ src/homeUtil.js
@@ -1,3 +1,9 @@
+// import
+import displayURL from "./asset/display.png";
+import customerURL from "./asset/customer.png";
+import couponURL from "./asset/coupon.svg";
+
 export default () => {
-    
+    // create neccesary elements
+
 };
